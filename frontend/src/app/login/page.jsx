b8578dbd925c25bed6aee772012df4b8f9205a03
arakhita <arakhitaSabata635@gmail.com>
2026-03-14 15:03:30 +0530

    feat: create login page and after login redirect to dashbord

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password },
+        { withCredentials: true },
+      );
+
+      alert(res.data.message);
+
+      router.push("/dashboard");
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
