7797e0e34a3d841ab6ed4a61dd9d5bdd64731ff5
arakhita <arakhitaSabata635@gmail.com>
2026-03-14 13:41:08 +0530

    feat: connect to neon db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,26 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import pool from "./config/db.js";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
-);
-
-app.use(helmet());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API running");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Database error" });
+  }
 });
 
 export default app;
