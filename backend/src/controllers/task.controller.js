9bd990d7a8f7bb5ae76069f7a1dac5dd21bce130
arakhita <arakhitaSabata635@gmail.com>
2026-03-14 14:18:24 +0530

    feat: create update delete task controler created

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -22,3 +22,55 @@ export const createTask = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+//update task
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    const result = await pool.query(
+      `UPDATE tasks
+       SET title=$1, description=$2, status=$3
+       WHERE id=$4 AND user_id=$5
+       RETURNING *`,
+      [title, description, status, id, req.userId],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//delete task
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      `DELETE FROM tasks
+       WHERE id=$1 AND user_id=$2
+       RETURNING *`,
+      [id, req.userId],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.json({
+      message: "Task deleted",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
