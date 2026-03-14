58cd900e418651ddeee3456262f5a65e1205fd2a
arakhita <arakhitaSabata635@gmail.com>
2026-03-14 15:01:07 +0530

    feat: setup frontend and create basic resister ui

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import { authMiddleware } from "./middleware/auth.middleware.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
---
+++ frontend/src/app/register/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/register", {
+        email,
+        password,
+      });
+
+      alert(res.data.message);
+    } catch (error) {
+      alert(error.response?.data?.message || "Error");
+    }
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
